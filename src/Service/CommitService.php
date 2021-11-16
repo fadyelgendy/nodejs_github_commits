a7baba40b3abd916048170937b89850bb5b3013b
fadyelgendy916 <69862698+fadyelgendy916@users.noreply.github.com>
2021-11-16 19:15:46 +0200

    Make sure no duplicates saved to DB

--- src/Service/CommitService.php
+++ src/Service/CommitService.php
@@ -18,18 +18,24 @@ class CommitService
     {
         if (count($commits)) {
             foreach($commits as $item) {
-                $commit = new Commit();
-                $commit->setSha($item['sha']);
-                $commit->sethtml_url($item['html_url']);
-                $commit->setauthor_name($item['commit']['author']['name']);
-                $commit->setauthor_email($item['commit']['author']['email']);
-                $commit->setauthor_url($item['author']['html_url']);
-                $commit->setcommitter_name($item['commit']['committer']['name']);
-                $commit->setcommitter_email($item['commit']['committer']['email']);
-                $commit->setcommitter_url($item['committer']['html_url']);
-                $commit->setStatus($item['commit']['verification']['verified']);
+                $old = $this->entityManager
+                    ->getRepository(Commit::class)
+                    ->findOneBy(['sha'=> $item['sha']]);
 
-               $this->entityManager->persist($commit);
+                if (!$old) {
+                    $commit = new Commit();
+                    $commit->setSha($item['sha']);
+                    $commit->sethtml_url($item['html_url']);
+                    $commit->setauthor_name($item['commit']['author']['name']);
+                    $commit->setauthor_email($item['commit']['author']['email']);
+                    $commit->setauthor_url($item['author']['html_url']);
+                    $commit->setcommitter_name($item['commit']['committer']['name']);
+                    $commit->setcommitter_email($item['commit']['committer']['email']);
+                    $commit->setcommitter_url($item['committer']['html_url']);
+                    $commit->setStatus($item['commit']['verification']['verified']);
+
+                    $this->entityManager->persist($commit);
+                }
             }
         }
 
